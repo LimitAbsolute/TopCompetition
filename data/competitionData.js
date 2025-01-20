cf8c31433c9998f1988ca0dffa67714f16ae811e
LimitAbsolute <1749089175@qq.com>
2025-01-20 11:41:46 +0800

    比赛量级划分颜色

--- data/competitionData.js
+++ data/competitionData.js
@@ -609,4 +609,4 @@ const competitionData = [{
             ]
         }
     }
-]
+];
---
+++ data/weightListData.js
@@ -0,0 +1,7 @@
+const weightListData = [{
+    type: "success",
+    text: "50-60g轻量级"
+}, {
+    type: "primary",
+    text: "50-70g轻中量级"
+}];
